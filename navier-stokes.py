c432dde8783068263dafb4babaa0447efd290119
bugsmasher6174 <41974437+bugsmasher6174@users.noreply.github.com>
2019-06-09 17:13:11 -0400

    Add files via upload

--- navier-stokes.py
+++ navier-stokes.py
@@ -4,20 +4,24 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as col
 import random
-l2 = [10,10,10,10,10,11,11,11,11,11,9,9,9,9,8,8,12,12,12,12,13,13,13,13,14,14,14,15,15,16,16,17,18]
-l1 = [8,9,10,11,12,8,9,10,11,12,8,9,10,11,9,10,8,9,10,11,8,9,10,11,8,9,10,8,9,8,9,8,8]
+
+l2 = [10,10,10,10,10,11,11,11,11,11,9,9,9,9,8,8,12,12,12,12,13,13,13,13,14,14,14,15,15,15,16,16,17,17,18,19]
+l1 = [8,9,10,11,12,8,9,10,11,12,8,9,10,11,9,10,8,9,10,11,8,9,10,11,8,9,10,8,9,10,8,9,8,9,8,8]
+
+'''l1=[10,11,12,13,14,15,16,17,18,19,20,21,22,16,16,16]
+l2=[8,8,9,9,10,10,10,10,10,9,9,8,8,11,12,13]'''
 mu=float(input('how viscous?'))
-a = numpy.full((30,30),-0.001)
-b = numpy.full((30,30),0)
-rho = numpy.full((30,30),2000)
-d = numpy.full((30,30),10)
-e = numpy.full((30,30),0.005)
-d3 = numpy.full((30,30),10)
-nono = numpy.zeros((30,30))
-div = numpy.zeros((30,30))
-gradi = numpy.zeros((30,30))
-gradj = numpy.zeros((30,30))
-laplacian = numpy.zeros((30,30))
+a = numpy.full((301,301),-30)
+b = numpy.full((301,301),0)
+rho = numpy.full((301,301),2000)
+d = numpy.full((301,301),10)
+e = numpy.full((301,301),0.005)
+d3 = numpy.full((301,301),10)
+nono = numpy.zeros((301,301))
+div = numpy.zeros((301,301))
+gradi = numpy.zeros((301,301))
+gradj = numpy.zeros((301,301))
+laplacian = numpy.zeros((301,301))
 for z in range(len(l2)):
   i=l1[z]
   j=l2[z]
@@ -29,11 +33,11 @@ for z in range(len(l2)):
     
   
       
-for t in range(50):
-  plt.pcolormesh(d,cmap=cm.Greys,vmin=9.7,vmax=10.3)
+for t in range(500):
+  plt.pcolormesh(d-nono*5,cmap=cm.Greys,vmin=7.001,vmax=13.001)
   plt.show()
-  for i in range(30):
-    for j in range(30):
+  for i in range(300):
+    for j in range(300):
       a2=1
       b2=1
       c2=1
@@ -54,8 +58,8 @@ for t in range(50):
         div[i,j]=((((((b[i3,j])-(b[i2,j]))/2))+((((a[i,j3])-(a[i,j2]))/2))))
         gradi[i,j]=((b[i3,j])-(b[i2,j]))
         gradj[i,j]=((a[i,j3])-(a[i,j2]))
-  for i in range(30):
-    for j in range(30):
+  for i in range(300):
+    for j in range(300):
       a2=1
       b2=1
       c2=1
@@ -73,21 +77,21 @@ for t in range(50):
       j2=j+c2
       j3=j-d2
       if nono[i,j]==0:
-        laplacian[i,j]=(((((((b[i3,j])-(b[i2,j]))/4)**2))+(((((a[i,j3])-(a[i,j2]))/4)**2))))
-  for i in range(30):
-    for j in range(30):
+        laplacian[i,j]=(((((((gradi[i3,j])-(gradi[i2,j]))/2)))+(((((gradj[i,j3])-(gradj[i,j2]))/2)))))
+  for i in range(300):
+    for j in range(300):
       a2=1
       b2=1
       c2=1
       d2=1
       if i==0:
-        b2=-29
-      if i==29:
-        a2=-29
+        b2=-299
+      if i==299:
+        a2=-299
       if j==0:
-        d2=-29
-      if j==29:
-        c2=-29
+        d2=-299
+      if j==299:
+        c2=-299
       i2=i+a2
       i3=i-b2
       j2=j+c2
@@ -95,23 +99,23 @@ for t in range(50):
       if nono[i,j]==0:
         x=rho[i,j]
         x2=d[i,j]
-        a[i,j]=(-(((d[i,j3])-(d[i,j2])))+mu*laplacian[i,j])/x+a[i,j]
-        b[i,j]=(-(((d[i3,j])-(d[i2,j])))+mu*laplacian[i,j])/x+b[i,j]
+        a[i,j]=(((((-((d[i,j3])-(d[i,j2])))+mu*laplacian[i,j]+(mu/3)*((div[i,j3])-(div[i,j2])))))/x)+a[i,j]
+        b[i,j]=(((((-((d[i3,j])-(d[i2,j])))+mu*laplacian[i,j]+(mu/3)*((div[i3,j])-(div[i2,j])))))/x)+b[i,j]
 
-  for i in range(30):
-    for j in range(30):
+  for i in range(300):
+    for j in range(300):
       a2=1
       b2=1
       c2=1
       d2=1
       if i==0:
-        b2=-29
-      if i==29:
-        a2=-29
+        b2=-299
+      if i==299:
+        a2=-299
       if j==0:
-        d2=-29
-      if j==29:
-        c2=-29
+        d2=-299
+      if j==299:
+        c2=-299
       i2=i+a2
       i3=i-b2
       j2=j+c2
@@ -119,6 +123,28 @@ for t in range(50):
       x=rho[i,j]
       x2=d[i,j]
       if nono[i,j]==0:
-        rho[i,j]=((((((b[i3,j])-(b[i2,j]))))+((((a[i,j3])-(a[i,j2]))))))*(-x)+x
-        e[i,j]=(((((b[i3,j])-(b[i2,j]))))+((((a[i,j3])-(a[i,j2])))))*-(d[i,j]/rho[i,j])+e[i,j]
+        rho[i,j]=(((((((b[i3,j])-(b[i2,j]))))+((((a[i,j3])-(a[i,j2])))))/500)*(-x))+x
+        e[i,j]=((((((b[i3,j])-(b[i2,j]))))+((((a[i,j3])-(a[i,j2]))))/500)*-(d[i,j]/rho[i,j]))+e[i,j]
         d[i,j]=e[i,j]*rho[i,j]
+  for i in range(300):
+    for j in range(300):
+      a2=1
+      b2=1
+      c2=1
+      d2=1
+      if i==0:
+        b2=-299
+      if i==299:
+        a2=-299
+      if j==0:
+        d2=-299
+      if j==299:
+        c2=-299
+      i2=i+a2
+      i3=i-b2
+      j2=j+c2
+      j3=j-d2
+      x2=d[i,j]
+      if nono[i,j]==0:
+         d3[i,j]=(x2+(d[i3,j]+d[i2,j]+d[i,j2]+d[i,j3]))/5
+  d=d3
